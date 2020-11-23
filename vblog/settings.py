a5edaaef8d06a1a4d194da922a7bb05aa84dac36
SkrejetCepey <skorpion.425358@gmail.com>
2020-11-23 17:25:12 +1000

    whatthe

--- vblog/settings.py
+++ vblog/settings.py
@@ -29,13 +29,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 # Yaroslav
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# import dj_database_url
-#
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
